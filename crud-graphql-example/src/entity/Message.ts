28e527455e51615201b192498999e2589d864a17
Samuel Hepworth <samhep0803@gmail.com>
2021-03-20 23:24:18 +0400

    begin work on conversations

---
+++ crud-graphql-example/src/entity/Message.ts
@@ -0,0 +1,32 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	BaseEntity,
+	CreateDateColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity("messages")
+export class Message extends BaseEntity {
+	@Field()
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Field()
+	@CreateDateColumn({ type: "timestamp with time zone" })
+	createdAt: Date;
+
+	@Field({ nullable: true })
+	@Column({ type: "timestamp", nullable: true })
+	updatedAt?: Date;
+
+	@Field()
+	@Column()
+	content: string;
+
+	@Field()
+	@Column()
+	authorId: number;
+}
